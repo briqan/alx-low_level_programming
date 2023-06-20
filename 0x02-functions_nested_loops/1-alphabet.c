bca5435db4c9da111e07185cb73036f40dbea23b
briqan <briqan19@gmail.com>
2023-06-20 03:36:10 -0700

    tyty

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * print_alphabet: prints .
+ */
+void print_alphabet(void)
+{
+	int j;
+
+	for ((j >= 97) && (j <= 122); j++;)
+	{
+		/**
+		 * print_alphabet-prints alphabets
+		 */
+		_putchar('j');
+	}
+	_putchar('\n');
+
+}
