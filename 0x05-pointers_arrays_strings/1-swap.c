caf64425f18e675ecae31e82d921b40f305a40b6
briqan <briqan19@gmail.com>
2023-06-26 04:48:43 -0700

    truer

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+
+}
